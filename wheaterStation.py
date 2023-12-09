176e3be865fa5b6d27f77912f2798a6bf1f84fc4
Bartosz Kulpa <bartek.kulpa95@gmail.com>
2023-12-09 20:56:29 +0100

    added comments

--- wheaterStation.py
+++ wheaterStation.py
@@ -1,7 +1,9 @@
+# Import niezbędnych bibliotek
 import mysql.connector
 import urllib.request
 import xml.etree.ElementTree as ET
 
+# Blok try-except mający na celu zapobieganie całkowitego zatrzymania sie programu i wyświetleniu komunikatów w przypadku wystąpienia błędów
 try:
     # Połączenie z bazą danych
     db = mysql.connector.connect(
@@ -11,15 +13,18 @@ try:
         database="sys"
     )
 
+    # 
+    cursor = db.cursor()
+
     # Tworzenie tabeli, jeśli nie istnieje
     createTableQuery = """CREATE TABLE IF NOT EXISTS POGODA_W_POLSCE (
       id INT unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
       id_stacji INT,
       stacja VARCHAR(255),
       data_pomiaru DATE,
-      godzina_pomiaru TIME,
+      godzina_pomiaru VARCHAR(10),
       temperatura FLOAT,
-      predkosc_wiatru INT,
+      predkosc_wiatru FLOAT,
       kierunek_wiatru INT,
       wilgotnosc_wzgledna FLOAT,
       suma_opadu FLOAT,
@@ -27,7 +32,7 @@ try:
       roznica_cisnien FLOAT
     );"""
 
-    cursor = db.cursor()
+    # Wykonanie zapytania ze zmiennej createTableQuery
     cursor.execute(createTableQuery)
 
     # Pobieranie danych z API i parsowanie XML
@@ -37,6 +42,7 @@ try:
     xmlTree = ET.fromstring(xmlAsString)
     foundMeasurement = xmlTree.findall("item")
 
+    # Dodawanie rekordów ze sparsowanego XML
     for measurement in foundMeasurement:
         try:
             id_stacji = measurement.find("id_stacji").text
@@ -50,12 +56,13 @@ try:
             suma_opadu = measurement.find("suma_opadu").text
             cisnienie = measurement.find("cisnienie").text
 
+            # Wyliczanie od różnicy ciśnienia wzorcowego tylko jeśli pole cisnienie nie przyjmuje wartości NULL
             if cisnienie:
                 roznica_cisnien = 1013.25 - float(cisnienie)
             else:
                 roznica_cisnien = None
 
-            # Wstawianie rekordu do bazy danych
+            #  Zapisanie rekordu do wstwanienia do bazy danych do zmiennej
             recordToInsert = """INSERT INTO POGODA_W_POLSCE (
                 id_stacji,
                 stacja, 
@@ -70,21 +77,26 @@ try:
                 roznica_cisnien) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s
             )"""
 
+            # Wstawienie rekordu ze zmiennej recordToInsert do bazy danych 
             cursor.execute(recordToInsert, (
                 id_stacji, stacja, data_pomiaru, godzina_pomiaru, temperatura, predkosc_wiatru, kierunek_wiatru,
                 wilgotnosc_wzgledna, suma_opadu, cisnienie, roznica_cisnien))
+            
+        # Błąd podczas dodawania rekordu do tabeli
         except Exception as e:
             print(f"Błąd podczas przetwarzania rekordu: {e}")
 
     # Zatwierdzenie zmian w bazie danych
     db.commit()
 
+# Błąd związany z biblioteką mysql.connector - na przykład błąd połączenia z bazą danych
 except mysql.connector.Error as err:
     print(f"Błąd MySQL: {err}")
+# Błąd niezwiązany z biblioteką mysql.connector - na przykład przy próbie dopisania rekordów do nieistjącej tabeli
 except Exception as e:
     print(f"Wystąpił błąd: {e}")
+# Zamknięcie połączenia z bazą danych
 finally:
-    # Zamknięcie połączenia z bazą danych
     if db.is_connected():
         cursor.close()
         db.close()
