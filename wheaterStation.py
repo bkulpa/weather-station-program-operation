adb41039cfe58747becca5fb27c5196bca96e85c
Bartosz Kulpa <bartek.kulpa95@gmail.com>
2023-12-11 00:56:32 +0100

    refactor

--- wheaterStation.py
+++ wheaterStation.py
@@ -5,9 +5,7 @@ import os
 import urllib.request
 import xml.etree.ElementTree as ET
 
-# Import poleceń SQL
-from sqlQueries import *
-
+# Napisac w komentarzu czemu wykorzystana jest klasa
 class DatabaseConfig:
     def __init__(self):
         # Wczytanie zmiennych środowiskowych z pliku .env
@@ -22,20 +20,6 @@ class DatabaseConfig:
 # Utworzenie instancji klasy
 dbConfig = DatabaseConfig()
 
-# IMPLEMENTACJA KLASY - DO POPRAWY I ZASTOSOWANIA W KODZIE
-class WeatherMeasurement:
-    def __init__(self, measurement):
-        self.id_stacji = measurement.find("id_stacji").text
-        self.stacja = measurement.find("stacja").text
-        self.data_pomiaru = measurement.find("data_pomiaru").text
-        self.godzina_pomiaru = measurement.find("godzina_pomiaru").text
-        self.temperatura = measurement.find("temperatura").text
-        self.predkosc_wiatru = measurement.find("predkosc_wiatru").text
-        self.kierunek_wiatru = measurement.find("kierunek_wiatru").text
-        self.wilgotnosc_wzgledna = measurement.find("wilgotnosc_wzgledna").text
-        self.suma_opadu = measurement.find("suma_opadu").text
-        self.cisnienie = measurement.find("cisnienie").text
-
 # Przypisanie linku API do zmiennej
 apiUrl = "https://danepubliczne.imgw.pl/api/data/synop/format/xml"
 
@@ -45,12 +29,91 @@ def getAndParseXmlData(apiUrl):
     xmlAsString = xmlUrl.read()
     xmlTree = ET.fromstring(xmlAsString)
     foundMeasurement = xmlTree.findall("item")
-   
-    # Przekształcanie znalezionych pomiarów na obiekty
-    foundMeasurementObjects = [WeatherMeasurement(measurement) for measurement in foundMeasurement]
 
-    return foundMeasurementObjects
-    # return foundMeasurement
+    return foundMeasurement
+
+# Zapisanie sparsowanych danych z API do listy
+def parseMeasurement(measurement):
+        id_stacji = measurement.find("id_stacji").text
+        stacja = measurement.find("stacja").text
+        data_pomiaru = measurement.find("data_pomiaru").text
+        godzina_pomiaru = measurement.find("godzina_pomiaru").text
+        temperatura = measurement.find("temperatura").text
+        predkosc_wiatru = measurement.find("predkosc_wiatru").text
+        kierunek_wiatru = measurement.find("kierunek_wiatru").text
+        wilgotnosc_wzgledna = measurement.find("wilgotnosc_wzgledna").text
+        suma_opadu = measurement.find("suma_opadu").text
+        cisnienie = measurement.find("cisnienie").text
+
+        # Wyliczanie od różnicy ciśnienia wzorcowego tylko jeśli pole cisnienie nie przyjmuje wartości NULL
+        roznica_cisnien = 1013.25 - float(cisnienie) if cisnienie else None
+
+        return [
+            id_stacji,
+            stacja,
+            data_pomiaru,
+            godzina_pomiaru,
+            temperatura,
+            predkosc_wiatru,
+            kierunek_wiatru,
+            wilgotnosc_wzgledna,
+            suma_opadu,
+            cisnienie,
+            roznica_cisnien
+        ]
+
+# Tworzenie tabeli, jeśli nie istnieje
+createTableQuery = """CREATE TABLE IF NOT EXISTS POGODA_W_POLSCE (
+    id INT unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_stacji INT,
+    stacja VARCHAR(255),
+    data_pomiaru DATE,
+    godzina_pomiaru VARCHAR(10),
+    temperatura FLOAT,
+    predkosc_wiatru FLOAT,
+    kierunek_wiatru INT,
+    wilgotnosc_wzgledna FLOAT,
+    suma_opadu FLOAT,
+    cisnienie FLOAT,
+    roznica_cisnien FLOAT
+);"""
+
+
+#  Wstawienie rekordu do bazy danych z pominięciem duplikatów
+insertQuery = """INSERT INTO POGODA_W_POLSCE (
+    id_stacji,
+    stacja, 
+    data_pomiaru,
+    godzina_pomiaru,
+    temperatura,
+    predkosc_wiatru,
+    kierunek_wiatru,
+    wilgotnosc_wzgledna, 
+    suma_opadu,
+    cisnienie,
+    roznica_cisnien
+) SELECT * FROM (
+    SELECT 
+    %s as id_stacji,
+    %s as stacja,
+    %s as data_pomiaru,
+    %s as godzina_pomiaru,
+    %s as temperatura,
+    %s as predkosc_wiatru,
+    %s as kierunek_wiatru,
+    %s as wilgotnosc_wzgledna,
+    %s as suma_opadu,
+    %s as cisnienie,
+    %s as roznica_cisnien
+) AS tmp WHERE NOT EXISTS (
+    SELECT * 
+    FROM POGODA_W_POLSCE 
+    WHERE 
+        id_stacji = tmp.id_stacji AND
+        data_pomiaru = tmp.data_pomiaru AND
+        godzina_pomiaru = tmp.godzina_pomiaru
+);"""
+
 
 # Blok try-except mający na celu zapobieganie całkowitego zatrzymania się programu i wyświetleniu komunikatów w przypadku wystąpienia błędów
 try:
@@ -72,28 +135,10 @@ try:
     # Dodawanie rekordów ze sparsowanego XML
     for measurement in foundMeasurement:
         try:
-            id_stacji = measurement.find("id_stacji").text
-            stacja = measurement.find("stacja").text
-            data_pomiaru = measurement.find("data_pomiaru").text
-            godzina_pomiaru = measurement.find("godzina_pomiaru").text
-            temperatura = measurement.find("temperatura").text
-            predkosc_wiatru = measurement.find("predkosc_wiatru").text
-            kierunek_wiatru = measurement.find("kierunek_wiatru").text
-            wilgotnosc_wzgledna = measurement.find("wilgotnosc_wzgledna").text
-            suma_opadu = measurement.find("suma_opadu").text
-            cisnienie = measurement.find("cisnienie").text
-
-            # Wyliczanie od różnicy ciśnienia wzorcowego tylko jeśli pole cisnienie nie przyjmuje wartości NULL
-            if cisnienie:
-                roznica_cisnien = 1013.25 - float(cisnienie)
-            else:
-                roznica_cisnien = None
-
+            parsedMeasurement = parseMeasurement(measurement)
             # Wykonanie polecenia ze zmiennej insertQuery
-            cursor.execute(insertQuery(), (
-                id_stacji, stacja, data_pomiaru, godzina_pomiaru, temperatura, predkosc_wiatru, kierunek_wiatru,
-                wilgotnosc_wzgledna, suma_opadu, cisnienie, roznica_cisnien))
-            
+            cursor.execute(insertQuery(), parsedMeasurement)            
+   
         # Błąd podczas dodawania rekordu do tabeli
         except Exception as e:
             print(f"Błąd podczas przetwarzania rekordu: {e}")
