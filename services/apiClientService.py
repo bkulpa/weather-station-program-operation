1c7fef19662589502c7929a9840dc76dae07b60c
Bartosz Kulpa <bartek.kulpa95@gmail.com>
2023-12-11 23:07:50 +0100

    refactor

--- services/apiClientService.py
+++ services/apiClientService.py
@@ -2,6 +2,8 @@ import os
 import urllib.request
 import xml.etree.ElementTree as ET
 
+STATUS_OK = 200
+
 # Pobieranie i parsowanie danych z API
 def getAndParseXmlData(path):
 
@@ -10,12 +12,13 @@ def getAndParseXmlData(path):
         xmlUrl = urllib.request.urlopen(os.getenv("API_URL") + path)
 
         # Sprawdzanie czy API zwraca status code 200
-        if xmlUrl.getcode() == 200:
+        if xmlUrl.getcode() == STATUS_OK:
             xmlAsString = xmlUrl.read()
             xmlTree = ET.fromstring(xmlAsString)
             foundMeasurement = xmlTree.findall("item")
         else:
-            print(f'Błąd: API zwróciło status code {xmlUrl.getcode()}')
+            # Zwrócenie błędu zawierającego status code z API
+            raise ValueError(f'Błąd: API zwróciło kod stanu {xmlUrl.getcode()}')
 
     # Błąd związany z API - na przykład w przypadku braku odpowiedzi z API
     except Exception as error:
